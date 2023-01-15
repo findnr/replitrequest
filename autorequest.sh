cf8c8ff31b40bf58e70b7ceb6f53fb1a81b4e321
cym504875043 <504875043@qq.com>
2023-01-15 13:08:12 +0800

    Update autorequest.sh
    增加两个地址

--- autorequest.sh
+++ autorequest.sh
@@ -1,3 +1,11 @@
 #!/bin/shell
+
+#文档服务器
 curl https://doc.cym504875043.repl.co
+#api服务器
 curl https://api.cym504875043.repl.co
+#聊天websocket服务器
+curl https://swoole.cym504875043.repl.co
+#聊天前端页面
+curl https://msghtml.cym504875043.repl.co
+
